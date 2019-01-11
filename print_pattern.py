ef51a20b0516c0be5665664a0751c81f1c78bb3e
Parul Bunkar <parulbunkar@Paruls-MacBook-Air.local>
2019-01-11 19:40:57 +0530

    added question

---
+++ print_pattern.py
@@ -0,0 +1,70 @@
+###
+#write a for or while loop to print as bellow: 
+"""1st pattern
+********
+*******
+******
+*****
+****
+***
+**
+*
+"""
+
+"""2nd pattern
+##########
+ ########
+  ######
+   ####
+    ##
+"""
+
+"""3rd pattern
+#########
+ ########
+  #######
+   ######
+    #####
+     ####
+      ###
+       ##
+        #
+"""
+
+"""4th pattern
+PrashantTiwari
+ rashantTiwar
+  ashantTiwa
+   shantTiw
+    hantTi
+     ant
+      n
+"""
+
+"""5th pattern
+P            i
+ r          r
+  a        a
+   s      w
+    h    i
+     a  T
+      nt
+"""
+
+"""6th pattern
+P            i
+ r          r
+  a        a
+   s      w
+    h    i
+     a  T
+      nt
+      nt
+     a  T
+    h    i
+   s      w
+      .
+      .
+      .
+"""
+
